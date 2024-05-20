7267bdcdbea473cf9af46e8e84e11ad1f22e8094
RBxRobson <robsonnbarbosa0@gmail.com>
2024-05-20 17:20:51 -0300

    fix: Ajusta erro na configuração do CORS

--- api/server.js
+++ api/server.js
@@ -1,18 +1,9 @@
 const jsonServer = require('json-server');
-const cors = require('cors');
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-const corsOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
-  credentials: true
-};
-
-server.use(cors(corsOptions));
 server.use(middlewares);
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
